39d787ce7477762578111a9e97657b157658e1ea
Chandrashekhar <48991661+shekhus@users.noreply.github.com>
2022-07-22 23:49:57 +0530

    set Explore NFTs, Listed NFTs,My NFTs as active

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,9 +6,34 @@ import Link from 'next/link';
 
 import images from '../assets';
 
+const MenuItems = ({ isMobile, active, setActive}) =>{ 
+  const generateLink = () => {
+  };
+
+  return (
+    <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
+      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
+        <li
+          key={i}
+          onClick={() => {}}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3
+          ${active === item
+            ? 'dark:text-white text-nft-black-1'
+            : 'dark:text-nft-gray-3 text-nft-gray-2'} 
+          `}
+        >
+        <Link href ="">{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 const Navbar = () => {
 
   const {theme,setTheme} = useTheme();
+  
+  const [active, setActive] = useState('Explore NFTs')
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
@@ -38,14 +63,20 @@ const Navbar = () => {
             id="checkbox"
             onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
           />
-          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
-            <i className="fas fa-sun" />
-            <i className="fas fa-moon" />
-            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
-          </label>
+            <label htmlFor="checkbox"       className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+              <i className="fas fa-sun" />
+              <i className="fas fa-moon" />
+              <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+            </label>
+          </div>
         </div>
 
+        <div className="md:hidden flex">
+          <ul className="list-none flexCenter flex-row">
+            <MenuItems active={active} setActive={setActive} />
+          </ul>
         </div>
+
     </nav>
   );
     
